901544f6cc5c90ec2c978f21641e38ef3fed52da
MrChoclateBar <bbbbbbbar@gmail.com>
2018-05-17 23:43:26 +0300

    improved code styling, added generic class handleing for future improvs

---
+++ pokemonDesrialize/src/com/anime/ObjectStream.java
@@ -0,0 +1,12 @@
+package com.anime;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ObjectStream extends Socket
+{
+    private ObjectStream(String localHost, int port)
+    {
+
+    }
+
+}
